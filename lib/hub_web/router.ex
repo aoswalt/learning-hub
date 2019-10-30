977ce07644e6fe2ef6699b5319eae4f48cc2f853
Adam Oswalt <email@adamoswalt.com>
2019-10-29 21:43:58 -0500

    Add parseltongue plug

--- lib/hub_web/router.ex
+++ lib/hub_web/router.ex
@@ -11,6 +11,7 @@ defmodule HubWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :parseltongue
   end
 
   scope "/", HubWeb do
@@ -26,5 +27,10 @@ defmodule HubWeb.Router do
     resources "/questions", QuestionController, except: [:new, :edit]
     resources "/profiles", ProfileController, except: [:new, :edit]
     resources "/answers", AnswerController, except: [:new, :edit]
+  end
+
+  defp parseltongue(conn, _opts) do
+    snake_params = Map.new(conn.params, fn {k, v} -> {Macro.underscore(k), v} end)
+    Map.put(conn, :params, snake_params)
   end
 end
