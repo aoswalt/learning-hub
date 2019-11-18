1274bcd3458f1207440947a9bcbbf8cea06e7d46
Adam Oswalt <email@adamoswalt.com>
2019-11-18 17:05:12 -0600

    Implement ResourceController in AnswerController
    Spec is not 100% because of string <-> NaiveDateTime

--- lib/hub_web/controllers/answer_controller.ex
+++ lib/hub_web/controllers/answer_controller.ex
@@ -1,12 +1,9 @@
 defmodule HubWeb.AnswerController do
-  use HubWeb, :controller
+  use HubWeb.ResourceController, for: HubDB.Answer
 
   import Norm
 
-  alias HubDB.Answer
-
-  action_fallback HubWeb.FallbackController
-
+  @impl HubWeb.ResourceController
   def resource_s(type \\ nil) do
     s =
       schema(%{
@@ -24,8 +21,8 @@ defmodule HubWeb.AnswerController do
           ),
         "createdAt" =>
           with_gen(
-            spec(&match?({:ok, %NaiveDateTime{}}, NaiveDateTime.from_iso8601(&1))),
-            HubDB.Spec.Generators.naive_datetime(true)
+            spec(&match?(%NaiveDateTime{}, &1)),
+            HubDB.Spec.Generators.naive_datetime()
           )
       })
 
@@ -36,46 +33,14 @@ defmodule HubWeb.AnswerController do
     end
   end
 
-  def index(conn, params) do
-    question_id = Map.get(params, "question_id")
-
-    answers =
-      if question_id do
-        Hub.list_answers_for_question(question_id)
-      else
-        Hub.list_answers()
-      end
-
-    render(conn, "index.json", answers: answers)
-  end
-
-  def create(conn, answer_params) do
-    with {:ok, %Answer{} = answer} <- Hub.create_answer(answer_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.answer_path(conn, :show, answer))
-      |> render("show.json", answer: answer)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    answer = Hub.get_answer!(id)
-    render(conn, "show.json", answer: answer)
-  end
-
-  def update(conn, %{"id" => id} = answer_params) do
-    answer = Hub.get_answer!(id)
-
-    with {:ok, %Answer{} = answer} <- Hub.update_answer(answer, answer_params) do
-      render(conn, "show.json", answer: answer)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    answer = Hub.get_answer!(id)
-
-    with {:ok, %Answer{}} <- Hub.delete_answer(answer) do
-      send_resp(conn, :no_content, "")
-    end
+  @impl HubWeb.ResourceController
+  def to_serializable(answer) do
+    %{
+      "id" => answer.id,
+      "text" => answer.text,
+      "questionId" => answer.question_id,
+      "createdAt" => answer.inserted_at,
+      "createdBy" => answer.created_by
+    }
   end
 end
